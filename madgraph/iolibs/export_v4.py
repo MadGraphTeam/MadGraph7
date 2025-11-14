b2d6895ed148ee16a024e691cdbfd91a197402ab
Olivier Mattelaer <olivier.mattelaer@uclouvain.be>
2025-11-14 14:21:34 +0100

    change f2py strategy for global library

--- madgraph/iolibs/export_v4.py
+++ madgraph/iolibs/export_v4.py
@@ -2213,6 +2213,8 @@ class ProcessExporterFortranSA(ProcessExporterFortran):
 
     matrix_template = "matrix_standalone_v4.inc"
     f2py_template = "matrix_standalone_f2py.inc"
+    f2py_wrapper_all ="f2py_wrapper_all.inc"
+    f2py_matrix_splitter = "f2py_splitter.py"
     jamp_optim = True
 
     def __init__(self, *args,**opts):
@@ -2398,197 +2400,14 @@ class ProcessExporterFortranSA(ProcessExporterFortran):
             ff = open(pjoin(self.dir_path, 'SubProcesses', 'makefile'),'a')
             ff.write(text)
             ff.close()
-                    
+    
+
     def write_f2py_splitter(self):
         """write a function to call the correct matrix element"""
-        
-        template = """
-%(python_information)s
-  subroutine smatrixhel(pdgs, procid, npdg, p, ALPHAS, SCALE2, nhel, ANS)
-  IMPLICIT NONE
-C ALPHAS is given at scale2 (SHOULD be different of 0 for loop induced, ignore for LO)  
 
-CF2PY double precision, intent(in), dimension(0:3,npdg) :: p
-CF2PY integer, intent(in), dimension(npdg) :: pdgs
-CF2PY integer, intent(in):: procid
-CF2PY integer, intent(in) :: npdg
-CF2PY double precision, intent(out) :: ANS
-CF2PY double precision, intent(in) :: ALPHAS
-CF2PY double precision, intent(in) :: SCALE2
-  integer pdgs(*)
-  integer npdg, nhel, procid
-  double precision p(*)
-  double precision ANS, ALPHAS, PI,SCALE2
-  include 'coupl.inc'
-  
-  
-  if (scale2.eq.0)then
-       PI = 3.141592653589793D0
-       G = 2* DSQRT(ALPHAS*PI)
-       CALL UPDATE_AS_PARAM()
-  else
-       CALL UPDATE_AS_PARAM2(scale2, ALPHAS)
-  endif
+        template = open(pjoin(MG5DIR, 'madgraph', 'iolibs', 'template_files', self.f2py_matrix_splitter)).read()
+        template2 = open(pjoin(MG5DIR, 'madgraph', 'iolibs', 'template_files', self.f2py_wrapper_all)).read()
 
-%(smatrixhel)s
-
-      return
-      end
-  
-      SUBROUTINE INITIALISE(PATH)
-C     ROUTINE FOR F2PY to read the benchmark point.
-      IMPLICIT NONE
-      CHARACTER*512 PATH
-CF2PY INTENT(IN) :: PATH
-      CALL SETPARA(PATH)  !first call to setup the paramaters
-      RETURN
-      END
-      
-      
-      subroutine CHANGE_PARA(name, value)
-      implicit none
-CF2PY intent(in) :: name
-CF2PY intent(in) :: value
-
-      character*512 name
-      double precision value
-      
-      %(helreset_def)s
-
-      include '../Source/MODEL/input.inc'
-      include '../Source/MODEL/coupl.inc'
-
-      %(helreset_setup)s
-
-      SELECT CASE (name)
-         %(parameter_setup)s
-         CASE DEFAULT
-            write(*,*) 'no parameter matching', name, value
-      END SELECT
-
-      return
-      end
-      
-    subroutine update_all_coup()
-    implicit none
-     call coup()
-    return 
-    end
-      
-
-    subroutine get_pdg_order(PDG, ALLPROC)
-  IMPLICIT NONE
-CF2PY INTEGER, intent(out) :: PDG(%(nb_me)i,%(maxpart)i)  
-CF2PY INTEGER, intent(out) :: ALLPROC(%(nb_me)i)
-  INTEGER PDG(%(nb_me)i,%(maxpart)i), PDGS(%(nb_me)i,%(maxpart)i)
-  INTEGER ALLPROC(%(nb_me)i),PIDs(%(nb_me)i)
-  DATA PDGS/ %(pdgs)s /
-  DATA PIDS/ %(pids)s /
-  PDG = PDGS
-  ALLPROC = PIDS
-  RETURN
-  END 
-
-    subroutine get_prefix(PREFIX)
-  IMPLICIT NONE
-CF2PY CHARACTER*20, intent(out) :: PREFIX(%(nb_me)i)
-  character*20 PREFIX(%(nb_me)i),PREF(%(nb_me)i)
-  DATA PREF / '%(prefix)s'/
-  PREFIX = PREF
-  RETURN
-  END 
- 
-
-
-    subroutine set_fixed_extra_scale(new_value)
-    implicit none
-CF2PY logical, intent(in) :: new_value
-    logical new_value
-                logical fixed_extra_scale
-            integer maxjetflavor
-            double precision mue_over_ref
-            double precision mue_ref_fixed
-            common/model_setup_running/maxjetflavor,fixed_extra_scale,mue_over_ref,mue_ref_fixed
-  
-        fixed_extra_scale = new_value
-        return 
-        end
-
-    subroutine set_mue_over_ref(new_value)
-    implicit none
-CF2PY double precision, intent(in) :: new_value
-    double precision new_value
-    logical fixed_extra_scale
-    integer maxjetflavor
-    double precision mue_over_ref
-    double precision mue_ref_fixed
-    common/model_setup_running/maxjetflavor,fixed_extra_scale,mue_over_ref,mue_ref_fixed
-  
-    mue_over_ref = new_value
-        
-    return 
-    end
-
-    subroutine set_mue_ref_fixed(new_value)
-    implicit none
-CF2PY double precision, intent(in) :: new_value
-    double precision new_value
-    logical fixed_extra_scale
-    integer maxjetflavor
-    double precision mue_over_ref
-    double precision mue_ref_fixed
-    common/model_setup_running/maxjetflavor,fixed_extra_scale,mue_over_ref,mue_ref_fixed
-  
-    mue_ref_fixed = new_value
-        
-    return 
-    end
-
-
-    subroutine set_maxjetflavor(new_value)
-    implicit none
-CF2PY integer, intent(in) :: new_value
-    integer new_value
-    logical fixed_extra_scale
-    integer maxjetflavor
-    double precision mue_over_ref
-    double precision mue_ref_fixed
-    common/model_setup_running/maxjetflavor,fixed_extra_scale,mue_over_ref,mue_ref_fixed
-  
-    maxjetflavor = new_value
-        
-    return 
-    end
-
-
-    subroutine set_asmz(new_value)
-    implicit none
-CF2PY double precision, intent(in) :: new_value
-    double precision new_value
-          integer nloop
-      double precision asmz
-      common/a_block/asmz,nloop
-    asmz = new_value
-    write(*,*) "asmz is set to ", new_value
-        
-    return 
-    end
-
-    subroutine set_nloop(new_value)
-    implicit none
-CF2PY integer, intent(in) :: new_value
-    integer new_value
-          integer nloop
-      double precision asmz
-      common/a_block/asmz,nloop
-    nloop = new_value
-     write(*,*) "nloop is set to ", new_value
-        
-    return 
-    end
-
-        """
-        
         allids = list(self.prefix_info.keys())
         allprefix = [self.prefix_info[key][0] for key in allids]
         min_nexternal = min([len(ids[0]) for ids in allids])
@@ -2654,7 +2473,11 @@ CF2PY integer, intent(in) :: new_value
         fsock = writers.FortranWriter(pjoin(self.dir_path, 'SubProcesses', 'all_matrix.f'),'w')
         fsock.writelines(text)
         fsock.close()
-    
+        text = template2 % formatting
+        fsock = writers.FortranWriter(pjoin(self.dir_path, 'SubProcesses', 'f2py_wrapper.f'),'w')
+        fsock.writelines(text)
+        fsock.close()    
+
     def get_model_parameter(self, model):
         """ returns all the model parameter
         """
